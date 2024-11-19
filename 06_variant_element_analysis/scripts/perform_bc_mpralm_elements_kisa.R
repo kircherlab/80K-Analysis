72a5b9f81e7c0ed9cfbd6ee528dbc7b9e5284c47
Kilian Salomon <kilian.salomon@bih-charite.de>
2024-11-19 11:14:32 +0100

    element and variant analysis script with bcalm

--- 06_variant_element_analysis/scripts/perform_bc_mpralm_elements_kisa.R
+++ 06_variant_element_analysis/scripts/perform_bc_mpralm_elements_kisa.R
@@ -1,16 +1,16 @@
 # input = 'data/ProxProm/raw_counts/HepG2_allreps_merged_barcode_assigned_counts.tsv.gz' # snakemake
 # install.packages("dplyr")
 # install.packages("devtools")
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager", repos = 'https://cran.uni-muenster.de/')
-BiocManager::install("mpra")
-install.packages("dplyr")
-install.packages("devtools")
-install.packages("tidyr")
+# if (!require("BiocManager", quietly = TRUE))
+#     install.packages("BiocManager", repos = 'https://cran.uni-muenster.de/')
+# BiocManager::install("mpra")
+# install.packages("dplyr")
+# install.packages("devtools")
+# install.packages("tidyr")
 # install ggplot
-install.packages("ggplot2")
+# library(mpra)
+# install.packages("ggplot2")
 library(ggplot2)
-library(mpra)
 library(dplyr)
 library(devtools)
 library(tidyr)
@@ -25,6 +25,8 @@ input = '/home/kisa/coding/80K_MPRA/element_analysis_output/element_assigned_bar
 input = '/home/kisa/coding/80K_MPRA/element_analysis_output/scrambled_vs_tested_element_assigned_barcodes.tsv.gz' # modified snakemake output (see: /home/kisa/coding/80K_MPRA/80K-Analysis/07_quality_control/notebooks/state_of_data.ipynb)
 input = '/home/kisa/coding/80K_MPRA/element_analysis_output/negative_neuron_NP_vs_tested_element_assigned_barcodes_no_alt.tsv.gz' # modified snakemake output (see: /home/kisa/coding/80K_MPRA/80K-Analysis/07_quality_control/notebooks/state_of_data.ipynb)
 input = '/home/kisa/coding/80K_MPRA/element_analysis_output/negative_neuron_CTRLs_vs_tested_element_assigned_barcodes_no_alt.tsv.gz' # modified snakemake output (see: /home/kisa/coding/80K_MPRA/80K-Analysis/07_quality_control/notebooks/state_of_data.ipynb)
+input = "/home/kisa/coding/80K_MPRA/server_results/MPRAsnakeflow/bbmap_standard_mapq10NoLength_unique_variant_id/NGN2_allreps_merged_barcode_assigned_counts.tsv.gz"
+# input = "/home/kisa/coding/80K_MPRA/server_results/MPRAsnakeflow/bbmap_standard_mapq10NoLength_unique_variant_id/testing_ngn2_MRPAsnakeflow_counts_302067.tsv.gz"
 
 bc_threshold <- 50
 bc_threshold <- 10
@@ -46,8 +48,10 @@ df_filt <- df %>% group_by(name) %>% filter(n() >= bc_threshold) %>% ungroup()
 nrow(df_filt)
 
 # number of oligos in file remaining:
-print("Number of unique names after filtering: ")
-length(unique(df_filt$name))
+print("Number of unique names after barcode filtering: ")
+print(length(unique(df_filt$name)))
+
+
 # element assigned: 27295
 # Filter: removing "ALT_" rows (not working)
 #df_filt_alt <- df_filt %>%
@@ -66,6 +70,8 @@ labelfile = '/data/cephfs-1/home/users/kisa11_c/unmirrored/projects/MPRA/IGVF_Y1
 labelfile = '/home/kisa/coding/80K_MPRA/design_data/removed_brackets_design_no_duplicates_sequence_and_header_label.fa'
 labelfile = '/home/kisa/coding/80K_MPRA/element_analysis_output/element_assigned_barcodes_label.tsv'
 labelfile = '/home/kisa/coding/80K_MPRA/element_analysis_output/negative_neuron_CTRLs_vs_tested_element_assigned_barcodes_no_alt_label_file.tsv'
+labelfile = "/home/kisa/coding/80K_MPRA/design_data/design_info/renamed_design_no_duplicates_sequence_and_header_with_adapter_no_brackets_no_collisions_label.tsv"
+
 labels <- read.table(labelfile, header=TRUE, sep='\t', col.names=c('name', 'label'))
 labels_vec <- as.vector(labels$label)
 names(labels_vec) <- labels$name
@@ -84,8 +90,9 @@ mpra <- MPRASet(DNA = dna, RNA = rna, eid = row.names(dna), barcode = NULL, labe
 
 # the replicate where each barcode belongs to is a blocking factor, indicated by the block_vector.
 block_vector <- rep(1:nr_reps, each=bcs)
-
+start <- Sys.time()
 mpralm_fit <- fit_elements(object = mpra, normalize=TRUE, block = block_vector)
+cat("running time: ", Sys.time() - start, "\n")
 
 # filter mpralm_fit by only having tested sequences and C_negative_neuron_NP
 # Right? (or are we interested in the performance of the controls?)
@@ -114,50 +121,62 @@ mpralm_fit <- fit_elements(object = mpra, normalize=TRUE, block = block_vector)
 # test_element <- mpralm_fit
 # test_element$neu_negative_np_normalized_logratio <- TRUE
 
-# define negative label: C_negative_neuron_NP
-test_mpra_treat_negative_np <- mpra_treat(mpralm_fit, 0.95, "C_negative_neuron_NP", "cardiac_neuro_cava_random", side="right")
-test_mpra_treat_positive_NP <- mpra_treat(mpralm_fit, 0.95, "C_positive_neuron_NP", "cardiac_neuro_cava_random", side="left")
-test_mpra_treat_mk_scrambled_only_two_groups <- mpra_treat(mpralm_fit, 0.95, "MK_scrambled", "cardiac_neuro_cava_random", side="both")
-test_mpra_treat_negative_neuron_NP_only_two_groups <- mpra_treat(mpralm_fit, 0.95, "C_negative_neuron_NP", "cardiac_neuro_cava_random", side="right")
-test_mpra_treat_negative_neuron_combined_controls_only_two_groups <- mpra_treat(mpralm_fit, 0.95, "C_combined_negative_neuron_MK_NP", "cardiac_neuro_cava_random", side="right")
+testing_element <- mpra_treat(mpralm_fit, 0.95, neg_label = "C_negative_neuron_NP", test_label = NULL, side = "right") # 68297 expected 69257 - 92
+View(testing_element)
+testing_element_both_sided <- mpra_treat(mpralm_fit, 0.95, neg_label = "C_negative_neuron_NP", test_label = NULL, side = "both") # 69165 expected 69257
 
-View(test_mpra_treat)
-View(test_mpra_treat_mk_scrambled_only_two_groups)
-View(test_mpra_treat_negative_neuron_NP_only_two_groups)
-View(test_mpra_treat_negative_neuron_combined_controls_only_two_groups)
-# with our without row names
-# most important: log; pvalue...
-# write.table(test_mpra_treat, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_elements_with_alt_all_controls.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
-write.table(test_mpra_treat_negative_np, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_elements_with_subset_controls_NP_neuro_negative_right_sided_tested.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
-write.table(test_mpra_treat_positive_NP, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_elements_with_subset_controls_NP_neuro_positive_left_sided_tested.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
-write.table(test_mpra_treat_mk_scrambled, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_elements_with_subset_controls_MK_scrambled_both_sided_tested.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
-write.table(test_mpra_treat_mk_scrambled_only_two_groups, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_MK_scrambled_vs_tested_elements_both_sided_no_alt.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
-write.table(test_mpra_treat_negative_neuron_NP_only_two_groups, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_negative_neuron_NP_vs_tested_elements_right_side_no_alt_unique.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
-write.table(test_mpra_treat_negative_neuron_combined_controls_only_two_groups, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_negative_neuron_combined_vs_tested_elements_right_side_no_alt.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+write.table(testing_element, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/testing_element_treat_result.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+toptab_allele_bc <- topTable(mpralm_fit, coef = 2, number = Inf)
+toptab_allele_bc$name <- row.names(toptab_allele_bc)
+write.table(toptab_allele_bc, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/testing_element_toptable_results.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+test_element_plot <- plot_groups(mpralm_fit, percentile = 0.95, neg_label = "C_negative_neuron_NP", test_label = "cardiac_neuro_cava_random")
+print(test_element_plot)
 
-# plot groups
-test_plotting_groups_negative_neuron_np <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "C_negative_neuron_NP", test_label = "cardiac_neuro_cava_random")
-test_plotting_groups_positive_neuron_np <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "C_positive_neuron_NP", test_label = "cardiac_neuro_cava_random")
-test_plotting_groups_mk_scrambled <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "MK_scrambled", test_label = "cardiac_neuro_cava_random")
-test_plotting_groups_mk_scrambled_two_groups <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "MK_scrambled", test_label = "cardiac_neuro_cava_random")
-test_mpra_treat_negative_neuron_NP_only_two_groups_two_groups <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "C_negative_neuron_NP", test_label = "cardiac_neuro_cava_random")
-test_plotting_negative_neuron_NP_MK_combined <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "C_combined_negative_neuron_MK_NP", test_label = "cardiac_neuro_cava_random")
+# # define negative label: C_negative_neuron_NP
+# test_mpra_treat_negative_np <- mpra_treat(mpralm_fit, 0.95, "C_negative_neuron_NP", "cardiac_neuro_cava_random", side="right")
+# test_mpra_treat_positive_NP <- mpra_treat(mpralm_fit, 0.95, "C_positive_neuron_NP", "cardiac_neuro_cava_random", side="left")
+# test_mpra_treat_mk_scrambled_only_two_groups <- mpra_treat(mpralm_fit, 0.95, "MK_scrambled", "cardiac_neuro_cava_random", side="both")
+# test_mpra_treat_negative_neuron_NP_only_two_groups <- mpra_treat(mpralm_fit, 0.95, "C_negative_neuron_NP", "cardiac_neuro_cava_random", side="right")
+# test_mpra_treat_negative_neuron_combined_controls_only_two_groups <- mpra_treat(mpralm_fit, 0.95, "C_combined_negative_neuron_MK_NP", "cardiac_neuro_cava_random", side="right")
 
-print(test_plotting_groups_negative_neuron_np)
-print(test_plotting_groups_positive_neuron_np)
-print(test_plotting_groups_mk_scrambled)
-print(test_plotting_groups_mk_scrambled_two_groups)
-print(test_plotting_groups_mk_scrambled_two_groups)
-print(test_mpra_treat_negative_neuron_NP_only_two_groups_two_groups)
-print(test_plotting_negative_neuron_NP_MK_combined)
+# View(test_mpra_treat)
+# View(test_mpra_treat_mk_scrambled_only_two_groups)
+# View(test_mpra_treat_negative_neuron_NP_only_two_groups)
+# View(test_mpra_treat_negative_neuron_combined_controls_only_two_groups)
+# # with our without row names
+# # most important: log; pvalue...
+# # write.table(test_mpra_treat, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_elements_with_alt_all_controls.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# write.table(test_mpra_treat_negative_np, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_elements_with_subset_controls_NP_neuro_negative_right_sided_tested.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# write.table(test_mpra_treat_positive_NP, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_elements_with_subset_controls_NP_neuro_positive_left_sided_tested.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# write.table(test_mpra_treat_mk_scrambled, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_elements_with_subset_controls_MK_scrambled_both_sided_tested.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# write.table(test_mpra_treat_mk_scrambled_only_two_groups, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_MK_scrambled_vs_tested_elements_both_sided_no_alt.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# write.table(test_mpra_treat_negative_neuron_NP_only_two_groups, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_negative_neuron_NP_vs_tested_elements_right_side_no_alt_unique.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# write.table(test_mpra_treat_negative_neuron_NP_only_two_groups, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_negative_neuron_NP_vs_all_tested_elements_right_side_unique.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# write.table(test_mpra_treat_negative_neuron_combined_controls_only_two_groups, file = "/home/kisa/coding/80K_MPRA/element_analysis_output/bc_mpralm_negative_neuron_combined_vs_tested_elements_right_side_no_alt.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+# # plot groups
+# test_plotting_groups_negative_neuron_np <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "C_negative_neuron_NP", test_label = "cardiac_neuro_cava_random")
+# test_plotting_groups_positive_neuron_np <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "C_positive_neuron_NP", test_label = "cardiac_neuro_cava_random")
+# test_plotting_groups_mk_scrambled <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "MK_scrambled", test_label = "cardiac_neuro_cava_random")
+# test_plotting_groups_mk_scrambled_two_groups <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "MK_scrambled", test_label = "cardiac_neuro_cava_random")
+# test_mpra_treat_negative_neuron_NP_only_two_groups_two_groups <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "C_negative_neuron_NP", test_label = "cardiac_neuro_cava_random")
+# test_plotting_negative_neuron_NP_MK_combined <- plot_groups(mpralm_fit, percentile = 0.95, neg_label= "C_combined_negative_neuron_MK_NP", test_label = "cardiac_neuro_cava_random")
+
+# print(test_plotting_groups_negative_neuron_np)
+# print(test_plotting_groups_positive_neuron_np)
+# print(test_plotting_groups_mk_scrambled)
+# print(test_plotting_groups_mk_scrambled_two_groups)
+# print(test_plotting_groups_mk_scrambled_two_groups)
+# print(test_mpra_treat_negative_neuron_NP_only_two_groups_two_groups)
+# print(test_plotting_negative_neuron_NP_MK_combined)
 
 
-# plot all labels: leave the neg_label
-test_plotting_all_groups <- plot_groups(mpralm_fit, percentile = 0.95, test_label = "cardiac_neuro_cava_random")
-print(test_plotting_all_groups)
+# # plot all labels: leave the neg_label
+# test_plotting_all_groups <- plot_groups(mpralm_fit, percentile = 0.95, test_label = "cardiac_neuro_cava_random")
+# print(test_plotting_all_groups)
 
-# plot all important labels:
-mpralm_fit_important_groups <- mpralm_fit %>%
-  filter(!grepl('GC', label))
+# # plot all important labels:
+# mpralm_fit_important_groups <- mpralm_fit %>%
+#   filter(!grepl('GC', label))
 
-test_plotting_important_groups <- plot_groups(mpralm_fit, percentile = 0.95, test_label = "cardiac_neuro_cava_random")
+# test_plotting_important_groups <- plot_groups(mpralm_fit, percentile = 0.95, test_label = "cardiac_neuro_cava_random")
--- 06_variant_element_analysis/scripts/perform_bc_mpralm_variants_kisa.R
+++ 06_variant_element_analysis/scripts/perform_bc_mpralm_variants_kisa.R
@@ -1,144 +1,103 @@
 # install.packages("dplyr")
 # install.packages("devtools")
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager", repos = 'https://cran.uni-muenster.de/')
-BiocManager::install("mpra")
-install.packages("dplyr")
-install.packages("devtools")
-install.packages("tidyr")
-# install ggplot
-install.packages("ggplot2")
+# if (!require("BiocManager", quietly = TRUE))
+#     install.packages("BiocManager", repos = 'https://cran.uni-muenster.de/')
+# BiocManager::install("mpra")
+# install.packages("dplyr")
+# install.packages("devtools")
+# install.packages("tidyr")
+# # install ggplot
+# install.packages("ggplot2")
+# library(mpra)
 library(ggplot2)
-library(mpra)
 library(dplyr)
 library(devtools)
+library(devtools)
 library(tidyr)
+library(arrow)
 
 load_all('/home/kisa/coding/80K_MPRA/bc_mpralm')
 
 # read input
-input = 'data/ProxProm/raw_counts/HepG2_allreps_merged_barcode_assigned_counts.tsv.gz'
-input = '/data/cephfs-1/home/users/kisa11_c/unmirrored/projects/MPRA/IGVF_Y1_design/experiment/final_resequencing/results/experiments/standard_bwa/assigned_counts/assignmentFixDuplicates/standardConfig/NGN2_allreps_merged_barcode_assigned_counts.tsv.gz' # snakemake
-input = '/data/cephfs-1/scratch/groups/kircher/MPRA/IGVF_Y1_design/experiment/09102024_80K_development_MPRAsnakeflow/results/experiments/bbmapStandardMapq35/assigned_counts/assignmentFixDuplicates/standardConfig/NGN2_allreps_merged_barcode_assigned_counts.tsv.gz' # snakemake
+# input = 'data/ProxProm/raw_counts/HepG2_allreps_merged_barcode_assigned_counts.tsv.gz'
+# input = '/data/cephfs-1/home/users/kisa11_c/unmirrored/projects/MPRA/IGVF_Y1_design/experiment/final_resequencing/results/experiments/standard_bwa/assigned_counts/assignmentFixDuplicates/standardConfig/NGN2_allreps_merged_barcode_assigned_counts.tsv.gz' # snakemake
+# input = '/data/cephfs-1/scratch/groups/kircher/MPRA/IGVF_Y1_design/experiment/09102024_80K_development_MPRAsnakeflow/results/experiments/bbmapStandardMapq35/assigned_counts/assignmentFixDuplicates/standardConfig/NGN2_allreps_merged_barcode_assigned_counts.tsv.gz' # snakemake
+input = "/data/cephfs-1/scratch/groups/kircher/MPRA/IGVF_Y1_design/experiment/09102024_80K_development_MPRAsnakeflow/unique_variant_current_development_bbmap_std_mapq35_80K_mpra_final_resequencing/experiments/bbmapStandardMapq35/assigned_counts/assignmentFixDuplicates/standardConfig/NGN2_allreps_merged_barcode_assigned_counts.tsv.gz" # snakemake # unique_variant_bbmap 35: 70500 => testable: 38362
+input = "/home/kisa/coding/80K_MPRA/server_results/MPRAsnakeflow/bbmap_standard_mapq10NoLength_unique_variant_id/NGN2_allreps_merged_barcode_assigned_counts.tsv.gz" # unique_variant_bbmap 10: 70798 => testable: 38589
+input = "/home/kisa/coding/80K_MPRA/server_results/MPRAsnakeflow/bbmap_standard_mapq10NoLength_unique_variant_id/testing_ngn2_MRPAsnakeflow_counts_302067.tsv.gz" # unique_variant_bbmap 10:  => testable: 323
+
 variant_map = "/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/resources/80k_Ngn2/renamed_variant_region_map_mprasnakeflow_input.tsv.gz"
+n_reps <- 3 # as.integer(args[5]) #
 
 # set output
 output_dir = '/home/kisa/coding/80K_MPRA/bc_MPRAlm_results/results/'
-output_name = 'toptable_bc_mpralm_80K_bbmap_std_mapq35.tsv'
+output_name = "bcalm_test_toptable_bc_mpralm_80K_bbmap_std_mapq35.tsv"
+output_name = "bcalm_test_toptable_bc_mpralm_80K_bbmap_std_mapq10.tsv"
+output_name = "testing_documentation_tmp_variant"
 
 # start preprocessing
 df <- read.table(file=gzfile(input), sep='\t', header=TRUE)
 
-# downsample barcode df
-df_smaller <- downsample_barcodes(df)
-dna <- create_dna_df(df_smaller, id_column_name="name")
-rna <- create_rna_df(df_smaller, id_column_name="name")
-
-map_df <- read.table(file=gzfile(variant_map), sep='\t', header=TRUE)
-map_df <- map_df %>% select(ID, REF, ALT)
-var_df <- create_var_df(df_smaller, map_df)
-
-# TODO: add optional filtering
-# ...
-
-
-
-# Create MPRASet
-# number of barcodes is number of columns, divided by (nr of samples * number of alleles)
-n_sample = 3
-bcs <- ncol(dna) / (n_sample*2)
-print('Call MPRASet ...')
-mpra_bc_set <- MPRASet(DNA = dna, RNA = rna, eid = row.names(dna), barcode = NULL)
-design <- data.frame(intcpt = 1, alt = grepl("alt", colnames(mpra_bc_set)))
-print('Finished with MPRASet')
-
-# the replicate where each barcode belongs to is a blocking factor, indicated by the block_vector.
-block_vector <- rep(1:n_sample, each=bcs*2)
-
-# Fit mpralm (using barcodes)
-print('Start with mpralm...')
-mpralm_fit_bc <- mpralm(object = mpra_bc_set, design = design, aggregate = "none", normalize = TRUE, block = block_vector, model_type = "corr_groups", plot = TRUE)
-#weights <- mpralm(object = mpra_bc_set, design = design, aggregate = "none", normalize = TRUE, model_type = "corr_groups", plot = TRUE, block = block_vector, return_weights=TRUE)
-print('Finished')
-print('Start writing output...')
-# Finding significant variants
-toptab_allele_bc <- topTable(mpralm_fit_bc, coef = 2, number = Inf)
-toptab_allele_bc$variant_id <- row.names(toptab_allele_bc)
-write.table(toptab_allele_bc, file=paste(output_dir, output_name, sep=""), sep="\t", row.names = FALSE)
-dev.off()
-
-toptab_allele_bc_not_na = toptab_allele_bc[!is.na(toptab_allele_bc$logFC),]
-toptab_allele_bc_not_na
-print('done')
-
-#### starting of pias code:
-library(devtools)
-suppressPackageStartupMessages(load_all("BCalm"))
-# suppressPackageStartupMessages(library(mpralm_bc))
-suppressPackageStartupMessages(library(arrow))
-suppressPackageStartupMessages(library(dplyr))
-
-args <- commandArgs(trailingOnly = TRUE)
-name <- "all" # args[1] #
-output <- "/data/humangen_kircherlab/MPRA/tt_mpra_analysis/results/HepG2_outputs/mpralm_bc/" # args[4] #
-nr_reps <- 3 # as.integer(args[5]) #
-
-
-# input = "/data/humangen_kircherlab/MPRA/tt_mpra_analysis/data/ProxProm/mpralm/mpralm_no_outliers_input_HepG2.tsv.gz"
-# df <- read.table(file=gzfile(input), sep='\t', header=TRUE)
-
-
-input <- "data/ProxProm/raw_counts/HepG2_allreps_merged_barcode_assigned_counts.tsv.gz"
-df <- read.table(file = gzfile(input), sep = "\t", header = TRUE)
+# filtering:
 df <- df %>% filter(if_all(matches("DNA", ignore.case = TRUE), ~ . >= 1))
 df <- df %>% filter(if_all(matches("RNA", ignore.case = TRUE), ~ . >= 1))
 
-variant_map <- "data/ProxProm/HepG2_variantTable.tsv.gz"
+print("Number of unique names loaded after dna filtering: ")
+print(length(unique(df$name)))
+
+# variant map
 map_df <- read.table(file = gzfile(variant_map), sep = "\t", header = TRUE)
 map_df <- map_df %>% select(ID, REF, ALT)
+# map_df[1,3]
 var_df <- create_var_df(df, map_df)
-
-# Function to detect outliers using the z-score method
-is_outlier <- function(x) {
-    z_scores <- (x - mean(x, na.rm = TRUE)) / sd(x, na.rm = TRUE)
-    abs(z_scores) > 3
-}
-# Filter out rows where any RNA count is an outlier
-# var_df <- var_df %>% filter(!if_any(all_of(grep("rna", colnames(df), value = TRUE)), is_outlier))
-
+colnames(df)
+colnames(map_df)
+print("Number of unique names loaded after create_var_df: ")
+print(length(unique(var_df$variant_id)))
 # Filter for oligos with at least min_bc barcodes in both alleles
+min_bc <- 10
 df_filt <- var_df %>%
     group_by(variant_id, allele) %>%
-    filter(n() >= 10) %>%
+    filter(n() >= min_bc) %>%
     ungroup()
 df_filt <- df_filt %>%
     group_by(variant_id) %>%
     filter(n_distinct(allele) == 2) %>%
     ungroup()
 
+print("Number of unique variants loaded after filtering for barcodes and allele existence: ")
+print(length(unique(df_filt$variant_id)))
 
-df_smaller <- downsample_barcodes(df_filt, "variant_id")
-dna <- create_dna_df(df_smaller)
-rna <- create_rna_df(df_smaller)
+# downsample barcode df
+df_smaller <- downsample_barcodes(df_filt, id_column_name = 'variant_id')
+dna <- create_dna_df(df_smaller, id_column_name = "variant_id")
+rna <- create_rna_df(df_smaller, id_column_name = "variant_id")
 
-s <- nr_reps
+n_sample <- n_reps
 
-# number of barcodes is number of columns, divided by (nr of samples * number of alleles)
-bcs <- ncol(dna) / (s * 2)
+# number of n_samples is number of columns, divided by (nr of samples * number of alleles)
+bcs <- ncol(dna) / (n_sample * 2)
 
+print("Call MPRASet ...")
 mpra <- MPRASet(DNA = dna, RNA = rna, eid = row.names(dna), barcode = NULL)
 design <- data.frame(intcpt = 1, alt = grepl("alt", colnames(mpra)))
+print("Finished with MPRASet")
+
 # the replicate where each barcode belongs to is a blocking factor, indicated by the block_vector.
-block_vector <- rep(1:s, each = bcs * 2)
+block_vector <- rep(1:n_sample, each = bcs * 2)
 
+print("Start with BCalm...")
 start <- Sys.time()
-
 mpralm_fit <- mpralm(object = mpra, design = design, aggregate = "none", normalize = TRUE, model_type = "corr_groups", plot = FALSE, block = block_vector)
 # weights <- mpralm(object = mpra, design = design, aggregate = "none", normalize = TRUE, model_type = "corr_groups", plot = TRUE, block = block_vector, return_weights=TRUE)
-cat("running time: ", Sys.time() - start, "\n")
+cat("Finished ... running time: ", Sys.time() - start, "\n")
 
+# write output:
+print("Start writing output...")
 # Finding significant variants
-toptab_allele <- topTable(mpralm_fit, coef = 2, number = Inf)
-toptab_allele$variant_id <- row.names(toptab_allele)
-# dev.off()
-# write_feather(toptab_allele, paste(output,"toptable_",name,"_HepG2.feather", sep=""))
+toptab_allele_bc <- topTable(mpralm_fit, coef = 2, number = Inf)
+toptab_allele_bc$variant_id <- row.names(toptab_allele_bc)
+write.table(toptab_allele_bc, file = paste(output_dir, output_name, sep = ""), sep = "\t", row.names = FALSE)
+# write_feather(toptab_allele_bc, paste(output_dir, output_name_feather, sep=""))
+
+print("done")
