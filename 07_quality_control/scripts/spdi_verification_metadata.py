3f22ad22779ed992baafeed5cd92641c2b90196a
Kilian Salomon <kilian.salomon@bih-charite.de>
2025-01-27 17:40:27 +0100

    final spdi verification on cluster

--- 07_quality_control/scripts/spdi_verification_metadata.py
+++ 07_quality_control/scripts/spdi_verification_metadata.py
@@ -79,6 +79,7 @@ def verify_spdi_result(verified_spdi_file, all_spdi_df, error_file):
     return not_matchable_spdis
 
 
+
 # file_list = [
 #  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/C_positive_heart_AB/C_positive_heart_AB.metadata.tsv.gz',
 #  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Glut_Chengyu/GC_Glut_Chengyu.metadata.tsv.gz',
@@ -111,12 +112,73 @@ def verify_spdi_result(verified_spdi_file, all_spdi_df, error_file):
 # '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/neuro_controls/C_positive_neuron_CD.metadata.tsv.gz',]
 # file_list = ['/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/neuro_controls/C_positive_neuron_CD.metadata.tsv.gz',]
 
+# # only the indel groups:
+# file_list = [
+#     '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/neuro_controls/C_positive_neuron_NP.metadata.tsv.gz',
+#     '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/neuro_controls/C_negative_heart_MK.metadata.tsv.gz',
+# ]
+
+# ## second run:
+# file_list= ['/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Liang/GC_Liang.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Mendelian_variants/GC_Mendelian_variants.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Hon/GC_Hon.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Mohlke/GC_Mohlke.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/C_positive_heart_CAD/C_positive_heart_CAD.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Cort_Chengyu/GC_Cort_Chengyu.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Kircher/GC_Kircher.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/cardiac_neuro_cava_random/cardiac_neuro_cava_random.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Atrial_fib/GC_Atrial_fib.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/C_negative_heart_MK/C_negative_heart_MK.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/C_positive_heart_AB/C_positive_heart_AB.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/C_positive_heart_MK/C_positive_heart_MK.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Vista/GC_Vista.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_GABA_Chengyu/GC_GABA_Chengyu.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Selvarajan/GC_Selvarajan.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/neuro_controls/C_negative_neuron_NP.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/neuro_controls/C_positive_neuron_NP.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/neuro_controls/C_positive_neuron_CD.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/neuro_controls/C_positive_neuron_MK.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/neuro_controls/C_negative_neuron_MK.metadata.tsv.gz',
+#  '/home/kisa/coding/80K_MPRA/MPRAOligoDesign/resources/controls/GC_Glut_Chengyu/GC_Glut_Chengyu.metadata.tsv.gz']
+
+
+# # on server:
+# file_list = ['/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Mohlke/GC_Mohlke.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/C_positive_heart_CAD/C_positive_heart_CAD.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/C_positive_heart_AB/C_positive_heart_AB.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Glut_Chengyu/GC_Glut_Chengyu.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/C_negative_heart_MK/C_negative_heart_MK.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Liang/GC_Liang.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_GABA_Chengyu/GC_GABA_Chengyu.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/SLEA/SLEA.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/DNase_NegControls/GC_DNase_negative_brain.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/DNase_NegControls/GC_DNase_negative_brain_shuffeled.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/DNase_NegControls/GC_DNase_negative_blood.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/DNase_NegControls/GC_DNase_negative_blood_shuffeled.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Mendelian_variants/GC_Mendelian_variants.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Vista/GC_Vista.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Cort_Chengyu/GC_Cort_Chengyu.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Hon/GC_Hon.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/C_positive_heart_MK/C_positive_heart_MK.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Selvarajan/GC_Selvarajan.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Kircher/GC_Kircher.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/DNase_PosControls/GC_DNase_positive_shuffeled.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/DNase_PosControls/GC_DNase_positive.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/GC_Atrial_fib/GC_Atrial_fib.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/neuro_controls/C_negative_neuron_MK.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/neuro_controls/C_positive_neuron_NP.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/neuro_controls/C_positive_neuron_MK.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/neuro_controls/C_negative_neuron_NP.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/neuro_controls/C_positive_neuron_CD.metadata.tsv.gz',
+#  '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/MK/MK.metadata.tsv.gz']
+
+# # on server #2:
+# file_list = ['/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/MK/MK.modified.metadata.tsv.gz']
 
 def main():
-    # file_list = list_metadata_files_in_subdirectories(directory_path, metadata_file_ending)
+    file_list = list_metadata_files_in_subdirectories(directory_path, metadata_file_ending)
     list_columns = [col_variant_class, col_variant_pos, col_SPDI, col_allele]
 
-    # file_list = ['/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/cardiac_neuro_cava_random/cardiac_neuro_cava_random.metadata.tsv.gz']
     for file in file_list:
         group_name = file.split('/')[-1].split(metadata_file_ending)[0]
         print(group_name)
@@ -131,16 +193,27 @@ def main():
         if alternatives_df.shape[0] == 0:
             continue
         output_directory = '/home/kisa/coding/80K_MPRA/80K-Analysis/07_quality_control/notebooks/control_metadata/spdi_verification' #os.path.abspath(os.getcwd())
+        output_directory = '/data/cephfs-2/unmirrored/groups/kircher/MPRA/IGVF_Y1_design/design/mpra_metadata_format/SPDI_verification' #os.path.abspath(os.getcwd())
         output_file = os.path.join(output_directory, f'SPDI_{group_name}.csv')
         verified_spdi_file = os.path.join(output_directory, f'verified_SPDI_{group_name}.csv')
-        alternatives_df[['SPDI']].to_csv(output_file, header=False, index=None)
+
+        if "MK/MK." in file:
+            # Extract all elements from the column of lists
+            SPDI_list = [item for spdi_list in alternatives_df['SPDI'] for item in spdi_list if len(spdi_list) > 0]
+
+            # Create a new DataFrame with these elements as a single column
+            SPDI_df = pd.DataFrame(SPDI_list, columns=['SPDI'])
+            # Write the DataFrame to a CSV file
+            SPDI_df[['SPDI']].to_csv(output_file, header=False, index=None)
+        else:
+            alternatives_df[['SPDI']].to_csv(output_file, header=False, index=None)
         # run the spdi batch checking script
         # Run a command and capture its output
         script_name = "/home/kisa/coding/80K_MPRA/igvf_spdi_demo_mike/spdi_batch.py"
         input_arg = f"-i {output_file}"
         command = [
             "python",
-            "/home/kisa/coding/80K_MPRA/igvf_spdi_demo_mike/spdi_batch.py",
+            script_name,
             "-i", output_file,
             "-t", "SPDI"
         ]
@@ -155,7 +228,7 @@ def main():
             alternatives_df.loc[alternatives_df[col_SPDI].isin(not_matchable_spdis)][['SPDI']].to_csv(output_file_second, header=False, index=None)
             command = [
             "python",
-            "/home/kisa/coding/80K_MPRA/igvf_spdi_demo_mike/spdi_batch.py",
+            script_name,
             "-i", output_file_second,
             "-t", "SPDI"
             ]
